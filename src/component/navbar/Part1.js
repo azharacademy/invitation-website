d87d2d016a885d1ccc4a7d199e5036836bc5016b
khalil-p <pkhalilmk@gmail.com>
2023-02-21 01:56:56 +0530

    fix:nav click open

--- src/component/navbar/Part1.js
+++ src/component/navbar/Part1.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img1 from '../images/burgurbtn.png'
 import img2 from '../images/Vector.png'
 import "./Navbar.css"
+import SideBar from '../sidebar/SideBar'
 function Part1() {
+    const [navOpen, setNavOpen] = useState(false)
     return (
         <>
             <div>
-                <div className='burgur_btn'>
+                <div className='burgur_btn' onClick={() => { !navOpen ? setNavOpen(true) : setNavOpen(false) }}>
                     <img src={img1} alt='' />
                     <h3>Invitation Plane</h3>
                 </div>
+                {navOpen ? <SideBar /> : ""}
                 <div className='search_section'>
                     <div className='input_search'>
                         <button className='search_button'><img src={img2} alt='' className='input_img' /></button>
